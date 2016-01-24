aff0ed43dad440dc63ed7bc181add21984daff2a
Yang Sopiana <yang.sopiana@gmail.com>
2016-01-24 09:08:14 +0700

    add javadoc to class_info.java

--- src/com/sopiana/yang/javaDecompiler/component/class_info.java
+++ src/com/sopiana/yang/javaDecompiler/component/class_info.java
@@ -1,6 +1,25 @@
 package com.sopiana.yang.javaDecompiler.component;
-
-public abstract class class_info {
-	int offset;
+/**
+ * The abstract class for <code>ClassFile</code>'s component
+ * 
+ * <p>It is needed to make debugging and comparing with class file easier, by providing
+ * the offset where each <code>ClassFile</code>'s component found.</p>
+ * @author yang.sopiana
+ *
+ */
+public abstract class class_info 
+{
+	/**
+	 * offset from class file data where specific component is found
+	 */
+	protected int offset;
+	
+	/**
+	 * return the offset from class file data where specific component is found
+	 * 
+	 * <p>The <code>offset</code> will give you the position of component in class file</p>
+	 * 
+	 * @return offset from class file data where specific component is found
+	 */
 	public int getOffset() { return offset; }
 }
