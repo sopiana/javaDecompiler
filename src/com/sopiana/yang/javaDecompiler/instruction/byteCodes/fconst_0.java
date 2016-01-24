5f7b95063ed129a79ea5c26c41602c6c183017d3
Yang Sopiana <yang.sopiana@gmail.com>
2016-01-24 17:05:56 +0700

    add dup2, dup2_x1, dup2_x2, f2d, f2i, f2l, fadd, faload, fastore, fcmpg, fcmpl, fconst_0, fconst_1, fconst_2

---
+++ src/com/sopiana/yang/javaDecompiler/instruction/byteCodes/fconst_0.java
@@ -0,0 +1,23 @@
+package com.sopiana.yang.javaDecompiler.instruction.byteCodes;
+
+import com.sopiana.yang.javaDecompiler.instruction.instruction;
+import com.sopiana.yang.javaDecompiler.instruction.instructionException;
+import com.sopiana.yang.javaDecompiler.instruction.opcodeTable;
+
+public class fconst_0 extends instruction
+{
+	public static final opcodeTable ins = opcodeTable._fconst_0;
+	
+	public static fconst_0 getInstance(byte[]codes, int offset) throws instructionException
+	{
+		if(codes[offset]!=ins.opcode)
+			throw new instructionException("supplied code is not valid "+ins.mnemonic+" opcode");
+		fconst_0 res = new fconst_0();
+		res.offset = offset;
+		res.opcode = codes[offset];
+		return res;
+	}
+	public byte[] getData() { return new byte[]{opcode}; }
+	public int getSize() { return 1; }
+	public String getMnemonic() { return ins.mnemonic; }
+}
---
+++ src/com/sopiana/yang/javaDecompiler/instruction/byteCodes/fconst_2.java
@@ -0,0 +1,23 @@
+package com.sopiana.yang.javaDecompiler.instruction.byteCodes;
+
+import com.sopiana.yang.javaDecompiler.instruction.instruction;
+import com.sopiana.yang.javaDecompiler.instruction.instructionException;
+import com.sopiana.yang.javaDecompiler.instruction.opcodeTable;
+
+public class fconst_2 extends instruction
+{
+	public static final opcodeTable ins = opcodeTable._fconst_2;
+	
+	public static fconst_2 getInstance(byte[]codes, int offset) throws instructionException
+	{
+		if(codes[offset]!=ins.opcode)
+			throw new instructionException("supplied code is not valid "+ins.mnemonic+" opcode");
+		fconst_2 res = new fconst_2();
+		res.offset = offset;
+		res.opcode = codes[offset];
+		return res;
+	}
+	public byte[] getData() { return new byte[]{opcode}; }
+	public int getSize() { return 1; }
+	public String getMnemonic() { return ins.mnemonic; }
+}
