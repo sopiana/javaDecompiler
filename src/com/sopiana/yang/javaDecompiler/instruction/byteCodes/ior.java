b60d656c30b059fbcf092674483e8683a356ca34
Yang Sopiana <yang.sopiana@gmail.com>
2016-01-24 20:38:31 +0700

    add ior, irem, ireturn, ishl, ishr, istore, istore_0, istore_1, istore_2, istore_3, isub, iushr

---
+++ src/com/sopiana/yang/javaDecompiler/instruction/byteCodes/ior.java
@@ -0,0 +1,23 @@
+package com.sopiana.yang.javaDecompiler.instruction.byteCodes;
+
+import com.sopiana.yang.javaDecompiler.instruction.instruction;
+import com.sopiana.yang.javaDecompiler.instruction.instructionException;
+import com.sopiana.yang.javaDecompiler.instruction.opcodeTable;
+
+public class ior extends instruction
+{
+	public static final opcodeTable ins = opcodeTable._ior;
+	
+	public static ior getInstance(byte[]codes, int offset) throws instructionException
+	{
+		if(codes[offset]!=ins.opcode)
+			throw new instructionException("supplied code is not valid "+ins.mnemonic+" opcode");
+		ior res = new ior();
+		res.offset = offset;
+		res.opcode = codes[offset];
+		return res;
+	}
+	public byte[] getData() { return new byte[]{opcode}; }
+	public int getSize() { return 1; }
+	public String getMnemonic() { return ins.mnemonic; }
+}
---
+++ src/com/sopiana/yang/javaDecompiler/instruction/byteCodes/ishl.java
@@ -0,0 +1,23 @@
+package com.sopiana.yang.javaDecompiler.instruction.byteCodes;
+
+import com.sopiana.yang.javaDecompiler.instruction.instruction;
+import com.sopiana.yang.javaDecompiler.instruction.instructionException;
+import com.sopiana.yang.javaDecompiler.instruction.opcodeTable;
+
+public class ishl extends instruction
+{
+	public static final opcodeTable ins = opcodeTable._ishl;
+	
+	public static ishl getInstance(byte[]codes, int offset) throws instructionException
+	{
+		if(codes[offset]!=ins.opcode)
+			throw new instructionException("supplied code is not valid "+ins.mnemonic+" opcode");
+		ishl res = new ishl();
+		res.offset = offset;
+		res.opcode = codes[offset];
+		return res;
+	}
+	public byte[] getData() { return new byte[]{opcode}; }
+	public int getSize() { return 1; }
+	public String getMnemonic() { return ins.mnemonic; }
+}
