c3ba5aa93598eb40044bbeb0fe5b55d66e4e827b
Yang Sopiana <yang.sopiana@gmail.com>
2016-01-31 20:12:24 +0700

    add toString method on LocalVariableTable_attribute

--- src/com/sopiana/yang/javaDecompiler/component/sub/attribute_info/LocalVariableTable_attribute.java
+++ src/com/sopiana/yang/javaDecompiler/component/sub/attribute_info/LocalVariableTable_attribute.java
@@ -4,12 +4,52 @@ import com.sopiana.yang.javaDecompiler.component.attribute_info;
 import com.sopiana.yang.javaDecompiler.component.cp_info;
 import com.sopiana.yang.javaDecompiler.component.sub.local_variable_table_info;
 import com.sopiana.yang.javaDecompiler.util.Util;
-
+/**
+ * Provides abstraction for <code>LocalVariableTable_attribute</code> item in <code>attributes</code> table
+ * 
+ * <p>The <code>LocalVariableTable_attribute</code> is an optional variable-length attribute in the <code>attributes</code> table of a 
+ * <code>Code_attribute</code>. It may be used by debuggers to determine the value of a given local variable during the execution of a method.</p>
+ * <p>If multiple <code>LocalVariableTable_attributes</code> are present in the <code>attributes</code> table of a <code>Code_attribute</code>, 
+ * then they may appear in any order.</p>
+ * <p>There may be no more than one <code>LocalVariableTable_attribute</code> per local variable in the <code>attributes</code> table of a 
+ * <code>Code_attribute</code>.</p>
+ * <p>The <code>LocalVariableTable_attribute</code> has the following format:</p>
+ * <code>LocalVariableTable_attribute {<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;u2 attribute_name_index;<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;u4 attribute_length;<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;u2 local_variable_table_length;<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;{ 
+ * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;u2 start_pc;<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;u2 length;<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;u2 name_index;<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;u2 descriptor_index;<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;u2 index;<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;} local_variable_table[local_variable_table_length];<br>
+ * }</code>
+ * @author yang.sopiana
+ *
+ */
 public class LocalVariableTable_attribute extends attribute_info
 {
+	/**
+	 * <p>The value of the <code>local_variable_table_length</code> item indicates the number of entries in the <code>local_variable_table array</code>.</p>
+	 */
 	private short local_variable_table_length;
+	/**
+	 * <p>Each entry in the <code>local_variable_table</code> array indicates a range of code array offsets within which a local variable has 
+	 * a value. It also indicates the index into the local variable array of the current frame at which that local variable can be found.</p>
+	 */
 	private local_variable_table_info local_variable_table[];	//local_variable_table_length
-	
+	/**
+	 * Factory method to generate a <code>LocalVariableTable_attribute</code> instance.
+	 * 
+	 * <p>The <code>LocalVariableTable_attribute</code> is an optional variable-length attribute in the <code>attributes</code> table of a 
+	 * <code>Code_attribute</code>. It may be used by debuggers to determine the value of a given local variable during the execution of a method.</p>
+	 * @param attribute_name_index attribute_name_index value representing the name of the attribute
+	 * @param attribute_length attribute_length value indicating the length of the subsequent information in bytes
+	 * @param info byte array from the class file
+	 * @return instance of <code>LocalVariableTable_attribute</code>
+	 */
 	public static LocalVariableTable_attribute getInstance(short attribute_name_index, int attribute_length, byte[]info) 
 	{
 		int offset = 0;
@@ -23,12 +63,35 @@ public class LocalVariableTable_attribute extends attribute_info
 			res.local_variable_table[i] = local_variable_table_info.getInstance(info, offset);
 			offset += res.local_variable_table[i].getSize();
 		}
+		
 		return res;
 	}
-
-	@Override
-	public String toString(int indent, cp_info[] constant_pool) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * Accessor method to <code>local_variable_table_length</code> field
+	 * 
+	 * <p>The value of the <code>local_variable_table_length</code> item indicates the number of entries in the <code>local_variable_table array</code>.</p>
+	 * @return value of <code>local_variable_table_length</code> field
+	 */
+	public short getLocal_variable_table_length() { return local_variable_table_length; }
+	/**
+	 * Accessor method to <code>local_variable_table</code> field
+	 * 
+	 * <p>Each entry in the <code>local_variable_table</code> array indicates a range of code array offsets within which a local variable has 
+	 * a value. It also indicates the index into the local variable array of the current frame at which that local variable can be found.</p>
+	 * @return value of <code>local_variable_table</code> field
+	 */
+	public local_variable_table_info[] getLocal_variable_table() { return local_variable_table; }
+	
+	public String toString(int indent, cp_info[] constant_pool) 
+	{
+		String indentStr = getIndent(indent);
+		String res=super.toString(indent, constant_pool);
+		res+=indentStr+" local_variable_table_length: "+local_variable_table_length+"\n";
+		for(int i=0;i<local_variable_table_length;++i)
+		{
+			res+=indentStr+" local_variable_table["+i+"]: \n";
+			res+=local_variable_table[i].toString(indent+1, constant_pool);
+		}
+		return res;
 	}
 }
--- src/com/sopiana/yang/javaDecompiler/component/sub/local_variable_table_info.java
+++ src/com/sopiana/yang/javaDecompiler/component/sub/local_variable_table_info.java
@@ -1,5 +1,6 @@
 package com.sopiana.yang.javaDecompiler.component.sub;
 
+import com.sopiana.yang.javaDecompiler.component.cp_info;
 import com.sopiana.yang.javaDecompiler.util.Util;
 
 public class local_variable_table_info {
@@ -24,5 +25,10 @@ public class local_variable_table_info {
 	public short getDescriptor_index() { return descriptor_index; }
 	public short getIndex() { return index; }
 	public int getSize() { return 10; }
+	public String toString(int indent, cp_info[] constant_pool) 
+	{	
+		//TODO finish this method
+		return null;
+	}
 }
 
